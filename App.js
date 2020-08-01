b44f70793b3c63c07b2dba360055f5fc1e90668e
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-01 10:59:02 +0430

    load fonts

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from "expo";
+
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'roboto-mono': require('./assets/fonts/RobotoMono-Regular.ttf'),
+        'roboto-mono-bold': require('./assets/fonts/RobotoMono-Bold.ttf'),
+        'roboto-mono-italic': require('./assets/fonts/RobotoMono-Italic.ttf')
+    });
+};
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
@@ -8,11 +18,18 @@ import GameOverScreen from "./screens/GameOverScreen";
 import Screens from "./models/Screens";
 
 const App = () => {
+    const [isLoad, setLoad] = useState(false);
+
     const [confirmedNumber, setConfirmedNumber] = useState(0);
 
     const [screen, setScreen] = useState(Screens.startGame);
 
     const [rounds, setRounds] = useState(0);
+
+    if (!isLoad)
+        return (
+            <AppLoading startAsync={fetchFonts} onFinish={() => setLoad(true)}/>
+        );
 
     const onStartGamePress = (number) => {
         setConfirmedNumber(number);
