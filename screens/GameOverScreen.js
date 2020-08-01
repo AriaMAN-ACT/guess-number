080dcd17fc1b3212df7c44bff9ef027e37d82d95
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-01 11:59:49 +0430

    show success image in game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -8,6 +8,9 @@ const GameOverScreen = ({rounds, userNumber, onPlayAgainPress}) => {
     return (
         <View style={styles.screen}>
             <Card style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image source={require('../assets/success.png')} style={styles.successImage} resizeMode="cover"/>
+                </View>
                 <Text style={styles.text}>Game Over In {rounds} Rounds!</Text>
                 <Text style={styles.text}>The Number Was:</Text>
                 <NumberContainer>{userNumber}</NumberContainer>
@@ -35,6 +38,21 @@ const styles = StyleSheet.create({
         marginTop: 10,
         fontSize: 16,
         fontFamily: 'roboto-mono'
+    },
+    imageContainer: {
+        width: 120,
+        height: 120,
+        borderRadius: 120,
+        overflow: 'hidden',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 3,
+        borderColor: '#FCF7F8DD',
+        marginVertical: 12
+    },
+    successImage: {
+        width: 150,
+        height: 150
     }
 });
 
